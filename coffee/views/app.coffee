98d375a0b534230c2934e18f533f68dd4c04a2b6
Gram Gibson <gram.gibson@gmail.com>
2011-08-14 10:24:51 -0400

    Add wrapper for localStorage API

--- coffee/views/app.coffee
+++ coffee/views/app.coffee
@@ -2,3 +2,10 @@ $ ->
 	window.socket = io.connect 'http://localhost:9000/'
 	root = new Feeds
 	root.init()
+	
+	window.store = (->
+	   set: (k,v) -> localStorage.setItem(k, v)
+	   get: (k) -> localStorage.getItem(k)
+	   remove: (k) -> localStorage.removeItem(k)
+	   clear: -> localStorage.clear()
+	)()
--- js/views/app.js
+++ js/views/app.js
@@ -3,6 +3,22 @@
     var root;
     window.socket = io.connect('http://localhost:9000/');
     root = new Feeds;
-    return root.init();
+    root.init();
+    return window.store = (function() {
+      return {
+        set: function(k, v) {
+          return localStorage.setItem(k, v);
+        },
+        get: function(k) {
+          return localStorage.getItem(k);
+        },
+        remove: function(k) {
+          return localStorage.removeItem(k);
+        },
+        clear: function() {
+          return localStorage.clear();
+        }
+      };
+    })();
   });
 }).call(this);
